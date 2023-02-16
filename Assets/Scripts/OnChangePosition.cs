f4e5e1823645867147d85afa275fa0b09822a240
MARS <61511540+MARSMPEI@users.noreply.github.com>
2023-02-16 21:47:40 +0300

    change code and some field

--- Assets/Scripts/OnChangePosition.cs
+++ Assets/Scripts/OnChangePosition.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class OnChangePosition : MonoBehaviour
 {
+    public PolygonCollider2D hole2DCollider;
+    public PolygonCollider2D ground2DCollider;
+    public MeshCollider GeneratedMeshCollider;
+    Mesh GeneratedMesh;
+
     // Start is called before the first frame update
-    void Start()
+    private void FixedUpdate()
     {
-        
+        if (transform.hasChanged == true)
+        {
+            transform.hasChanged = false;
+            hole2DCollider.transform.position = new Vector2(transform.position.x, transform.position.z);
+            MakeHole2D();
+            Make3DMeshCollider();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void MakeHole2D()
     {
-        
+        Vector2[] PointPosition = hole2DCollider.GetPath(0);
+        for (int i = 0; i < PointPosition.Length; i++)
+        {
+            PointPosition[i] += (Vector2)hole2DCollider.transform.position;
+        }
+        ground2DCollider.pathCount = 2;
+        ground2DCollider.SetPath(1, PointPosition);
+    }
+
+    private void Make3DMeshCollider()
+    {
+        if (GeneratedMesh != null)
+            Destroy(GeneratedMesh);
+        GeneratedMesh = ground2DCollider.CreateMesh(true, true);
+        GeneratedMeshCollider.sharedMesh = GeneratedMesh;
     }
 }
